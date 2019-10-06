1fc73bc00fd8c9b9caff200f911a23dfd8acb91f
Pavlo <44622067+pavlovpavlo@users.noreply.github.com>
2019-10-07 00:51:14 +0300

    Update 2019_10_04_111908_create_contacts_table.php

--- database/migrations/2019_10_04_111908_create_contacts_table.php
+++ database/migrations/2019_10_04_111908_create_contacts_table.php
@@ -15,7 +15,7 @@ class CreateContactsTable extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->timestamps();//отвечает за сохранение столбцов
+            $table->timestamps();
             $table->string('name');
             $table->string('phone');
             $table->text('description');
